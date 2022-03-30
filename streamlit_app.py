115b012bc7061851d99560ef31a52b1c54c015c7
root <root@p-a1cc5b2e-dc84-4930-b2bb-7f04ae78f6aa.projects-domain.projects.svc.cluster.local>
2022-03-30 17:10:02 +0000

    REPLACE THIS WITH YOUR COMMIT MESSAGE

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+
+##### This app is just an extremely simple example.
+##### See the Streamlit documentation for how to create more complex apps.
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the dataset
+baseball = pd.read_csv("player-batting-stats-1988-2016.csv")
+# constants for the position and team
+pos = "P"
+team = "BOS"
+# constant for batting statistic (RBI)
+batstat = "RBI"
+
+##### Title and intro
+
+st.title( 'Example Streamlit App' )
+
+
+##### Inputs
+
+st.header( 'Choose team and stat' )
+team = st.selectbox("Choose a team", baseball.Tm.unique())
+batstat = st.selectbox("Choose a batting stat", baseball.columns)
+
+
+##### Output
+baseball_filtered = baseball[baseball.Tm == team]
+baseball_sorted = baseball_filtered.sort_values(batstat, ascending=False)
+baseball_final = baseball_sorted.head(20)
+
+
+plt.hist(baseball_filtered[batstat], color="red")
+plt.ylabel("Frequency")
+plt.xlabel("Number of " + batstat)
+plt.title("Histogram for " + batstat + " for " + team)
+
+st.pyplot(plt.gcf())
